f00a1bbc04e8eb59c8ec07872ed9f58a295240f4
hhugohm <hhugohm@gmail.com>
2018-11-12 16:23:01 -0600

    add postman file

--- src/main/java/com/neosuniversity/demoweb/DemoWebApplication.java
+++ src/main/java/com/neosuniversity/demoweb/DemoWebApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoWebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoWebApplication.class, args);
 	}
