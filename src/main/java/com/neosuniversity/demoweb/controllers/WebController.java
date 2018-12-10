0219212dc6c0a79ddaf4b6dda57ea228d653c916
hhugohm <hhugohm@icloud.com>
2018-12-09 18:21:41 -0600

    add initial commit web

---
+++ src/main/java/com/neosuniversity/demoweb/controllers/WebController.java
@@ -0,0 +1,19 @@
+package com.neosuniversity.demoweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String getRoot(){
+
+        return "index";
+    }
+    @GetMapping("/admin")
+    public String getAdmin(){
+
+        return "index";
+    }
+}
